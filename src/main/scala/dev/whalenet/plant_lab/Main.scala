63ffd37252c38fea1fc6848a0f2226c3787fbe4f
qmastermoo <cpsaicontact@gmail.com>
2025-01-20 14:53:54 -0500

    working json handler

--- src/main/scala/dev/whalenet/plant_lab/Main.scala
+++ src/main/scala/dev/whalenet/plant_lab/Main.scala
@@ -1,5 +1,8 @@
 package dev.whalenet.plant_lab
 
+// could one day be needed
+//import io.circe.syntax._
+
 import cats.effect.*
 import org.http4s.*
 import org.http4s.circe.*
@@ -8,12 +11,13 @@ import org.http4s.ember.server.*
 import org.http4s.implicits.*
 import com.comcast.ip4s.*
 import org.typelevel.log4cats.slf4j.Slf4jFactory
+import io.circe.generic.auto._
 
 import java.time.ZonedDateTime
 
 
 
-//implicit val plant_decoder: EntityDecoder[IO, Plant] = jsonOf[IO, Plant]
+implicit val srd: EntityDecoder[IO, SensorResult] = jsonOf[IO, SensorResult]
 
 
 object Main extends IOApp {
@@ -24,14 +28,13 @@ object Main extends IOApp {
   implicit def catlogfactory: Slf4jFactory[IO] = Slf4jFactory.create[IO]
 
 
-  // private def sensorResultHandler(sensorResult: SensorResult): IO[Response[IO]] = {
-  private def sensorResultHandler(): IO[Response[IO]] = {
-    val person = Person(123, "tester")
-    val plant = Plant(456, "My Precious", "Tomato", person)
-    val sensor = Sensor(789, "Temperature", "Celsius")
-    val sensorResult = SensorResult(-1, plant, sensor, "100.0", ZonedDateTime.now)
-
-    Ok(s"$sensorResult")
+  private def sensorResultHandler(sensorResult: SensorResult): IO[Response[IO]] = {
+//    val person = Person(123, "tester")
+//    val plant = Plant(456, "My Precious", "Tomato", person)
+//    val sensor = Sensor(789, "Temperature", "Celsius")
+//    val sensorResult = SensorResult(-1, plant, sensor, "100.0", ZonedDateTime.now)
+    val persistedResult = sensorResult.copy(id=2468)
+    Ok(s"it worked $persistedResult")
   }
 
   val httpApp: HttpApp[IO] = HttpRoutes.of[IO] {
@@ -39,7 +42,8 @@ object Main extends IOApp {
       Ok(s"Hello, $name!")
     case req @ POST -> Root / "result" =>
       for {
-        resp <- sensorResultHandler()
+        result <- req.as[SensorResult]
+        resp <- sensorResultHandler(result)
       } yield resp
   }.orNotFound
 
--- src/main/scala/dev/whalenet/plant_lab/Models.scala
+++ src/main/scala/dev/whalenet/plant_lab/Models.scala
@@ -7,11 +7,11 @@ import java.time.ZonedDateTime
 // representation of a Person record
 case class Person (val id: Int, val name: String)
 
-case class Plant (val id: Int, val name: String, val plant_type: String, val owner: Person)
+case class Plant (val id: Int, val name: String, val plant_type: String, val owner_person_id: Int)
 
 case class Sensor(val id: Int, val name: String, val unit: String)
 
 
 // Plant_Sensor, but better name
 // storing value as string
-case class SensorResult(val id: Int, val plant: Plant, val sensor: Sensor, val value: String, val as_of: ZonedDateTime)
+case class SensorResult(val id: Int, val plant_id: Int, val sensor_id: Int, val value: String, val as_of: ZonedDateTime)
