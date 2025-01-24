d9fe668d34262b6c2ff03a20cd3afd08daa12241
qmastermoo <cpsaicontact@gmail.com>
2025-01-23 19:06:22 -0500

    add schema

--- src/main/scala/dev/whalenet/leaf_lab/Database.scala
+++ src/main/scala/dev/whalenet/leaf_lab/Database.scala
@@ -21,3 +21,77 @@ object DBConfig {
     println(s"Connected to database $dbUrl")
   }
 }
+
+/*
+/*
+Default SQL configuration
+
+Used to create initial version. Probably should have migrations somewhere, but this is what it is for now.
+ */
+
+create table if not exists lab.person
+(
+    id          serial
+        constraint person_pk
+            primary key,
+    keycloak_id varchar,
+    name        varchar                                                  not null
+);
+
+alter table lab.person
+    owner to leaf_lab_owner;
+
+create table if not exists lab.plant
+(
+    id        serial
+        constraint plant_pk
+            primary key,
+    person_id integer not null
+        constraint plant_person_id_fk
+            references lab.person,
+    name      varchar not null,
+    type      varchar not null
+);
+
+alter table lab.plant
+    owner to leaf_lab_owner;
+
+create index if not exists plant_person_id_index
+    on lab.plant (person_id);
+
+create table if not exists lab.sensor
+(
+    id   serial
+        constraint sensor_pk
+            primary key,
+    name varchar not null,
+    unit varchar not null
+);
+
+alter table lab.sensor
+    owner to leaf_lab_owner;
+
+create table if not exists lab.plant_sensor
+(
+    id        serial
+        constraint plant_sensor_pk
+            primary key,
+    plant_id  integer   not null
+        constraint plant_sensor_plant_id_fk
+            references lab.plant,
+    sensor_id integer   not null
+        constraint plant_sensor_sensor_id_fk
+            references lab.sensor,
+    as_of     timestamp not null,
+    value     varchar   not null
+);
+
+alter table lab.plant_sensor
+    owner to leaf_lab_owner;
+
+create index if not exists plant_sensor_plant_id_index
+    on lab.plant_sensor (plant_id);
+
+create index if not exists plant_sensor_sensor_id_index
+    on lab.plant_sensor (sensor_id);
+ */
